b3bedd73499848ca10a6ec7f40f708de88f79e51
WangZhishen <2632876982@qq.com>
2019-12-18 08:43:05 +0800

    aftermerge 1

--- src/main/java/com/bjfu/forestfiremonitor/controller/StatisticsController.java
+++ src/main/java/com/bjfu/forestfiremonitor/controller/StatisticsController.java
@@ -1,18 +1,37 @@
 package com.bjfu.forestfiremonitor.controller;
 
+import com.bjfu.forestfiremonitor.entity.Alarmrecord;
 import com.bjfu.forestfiremonitor.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class StatisticsController {
     @Autowired
     private StatisticsService statisticsService;
-    @GetMapping(value = "/getStatistics")
-    public String showStatistics(){
-        statisticsService.sortByYear();
+    @GetMapping(value = "/showStatistics")
+    public String showStatistics(Model model){
+
+        HashMap<String,Integer> countMap=statisticsService.getCountByYear();
+
+        model.addAttribute("countmap",countMap);
+
         return "ProfessorManagement/StatisticMain";
     }
+    @GetMapping(value = "/getByYearStatistics")
+    public String getByYearStatistics(Model model){
+
+        List<Alarmrecord> alarmrecordList=statisticsService.getAlarmrecordByYear("2018");
+
+        model.addAttribute("uid","123");
+
+        return "ProfessorManagement/StatisticMain";
+    }
+
 
 }
--- src/main/java/com/bjfu/forestfiremonitor/service/StatisticsService.java
+++ src/main/java/com/bjfu/forestfiremonitor/service/StatisticsService.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class StatisticsService {
     @Autowired
     AlarmrecordMapper alarmrecordMapper;
-    public void sortByYear()
+    public List<Alarmrecord> getAlarmrecordByYear(String targetYear)
     {
         List<Alarmrecord> alarmrecords=alarmrecordMapper.selectAll();
+        List<Alarmrecord> byyear=new ArrayList<Alarmrecord>();
         for(Alarmrecord alarmrecord : alarmrecords) {
             //得到年，月
             Date tempdate=alarmrecord.getAlarmtime();
@@ -27,8 +27,38 @@ public class StatisticsService {
 
             String[] splitdate=processdate.split(",");
             System.out.println(splitdate[0]+" and "+splitdate[1]);
-        }
+            if(targetYear.equals(splitdate[0])){
+                byyear.add(alarmrecord);
+            }
 
+        }
+        return byyear;
+    }
+    public HashMap<String, Integer> getCountByYear()
+    {
+        List<Alarmrecord> alarmrecords=alarmrecordMapper.selectAll();
+        HashMap<String, Integer> countbyyear=new HashMap<String, Integer>();
+
+        for(Alarmrecord alarmrecord : alarmrecords) {
+            //得到年，月
+            Date tempdate=alarmrecord.getAlarmtime();
+            DateFormat fmt = new SimpleDateFormat("yyyy,MM");
+            String processdate=fmt.format(tempdate);
+            String[] splitdate=processdate.split(",");
+            System.out.println(splitdate[0]+" and "+splitdate[1]);
+
+            if(countbyyear.containsKey(splitdate[0])) {
+                int value = countbyyear.get(splitdate[0]);
+                value++;
+                countbyyear.put(splitdate[0],value);
+            }
+            else {
+                int value=1;
+                countbyyear.put(splitdate[0],value);
+            }
+
+        }
+        return countbyyear;
     }
 
 
