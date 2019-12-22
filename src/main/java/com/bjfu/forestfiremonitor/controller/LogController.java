2cb2fb2cd0c67c526a6c53351bd5aca2b09cf289
WangZhishen <2632876982@qq.com>
2019-12-23 00:03:43 +0800

    WZS add sidebar right and merge zsh at 12/23 00：03

---
+++ src/main/java/com/bjfu/forestfiremonitor/controller/LogController.java
@@ -0,0 +1,45 @@
+package com.bjfu.forestfiremonitor.controller;
+
+import com.bjfu.forestfiremonitor.entity.Log;
+import com.bjfu.forestfiremonitor.entity.Picture;
+import com.bjfu.forestfiremonitor.service.AdminManagementService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class LogController {
+    @Autowired
+    AdminManagementService adminManagementService;
+
+    //用户表数据接口
+    @RequestMapping(value = "/getlogdata")
+    @ResponseBody
+    public String logtabledatainit() throws JsonProcessingException {
+        //fzj这获得所有用户的List
+        List<Log> allLogs = adminManagementService.getAllLog();
+        //其他不用做
+        //进行转json处理
+        String jsonString="{\"code\":0,\"message\":\"ok\", \"count\":"+allLogs.size()+",\"data\":";
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(allLogs);
+        jsonString+=json;
+        jsonString+="}";
+        //System.out.println(jsonString);
+        return jsonString;
+    }
+
+    @GetMapping(value = "/logtable")
+    public String logtable()
+    {
+        return "logtable";
+    }
+
+
+}
