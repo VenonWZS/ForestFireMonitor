e4e07b2b39195e88918e2ceb16b77eb811dc4295
fengzhongjie <fengzhongjie@bjfu.edu.cn>
2019-12-22 16:27:41 +0800

    fzj 2st

---
+++ src/main/java/com/bjfu/forestfiremonitor/service/MediaService.java
@@ -0,0 +1,30 @@
+package com.bjfu.forestfiremonitor.service;
+
+import com.bjfu.forestfiremonitor.dao.PictureMapper;
+import com.bjfu.forestfiremonitor.dao.VideoMapper;
+import com.bjfu.forestfiremonitor.entity.Picture;
+import com.bjfu.forestfiremonitor.entity.Video;
+import com.sun.xml.internal.org.jvnet.fastinfoset.stax.LowLevelFastInfosetStreamWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MediaService {
+
+    @Autowired
+    VideoMapper videoMapper;
+
+    @Autowired
+    PictureMapper pictureMapper;
+
+    public List<Video> getAllVideo(){
+        return videoMapper.allVideo();
+    }
+
+    public List<Picture> getAllPicture(){
+        return pictureMapper.allPicture();
+    }
+
+}
