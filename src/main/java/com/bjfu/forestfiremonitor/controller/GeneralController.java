79ff69eb59b64dfc87bce0928e605d3a34f2eaec
Dallarsz <dallarszh@gamil.com>
2020-01-01 10:13:15 +0800

    new zsh

--- src/main/java/com/bjfu/forestfiremonitor/controller/GeneralController.java
+++ src/main/java/com/bjfu/forestfiremonitor/controller/GeneralController.java
@@ -1,7 +1,13 @@
 package com.bjfu.forestfiremonitor.controller;
 
+import com.bjfu.forestfiremonitor.entity.POI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GeneralController {
@@ -22,8 +28,20 @@ public class GeneralController {
     public String zshpage() {return "ProfessorManagement/index";}
     @GetMapping("/zshshow")
     public String showzsh() {return "stasticShow";}
-    @GetMapping("/getlocation")
-    public String getlocation() {return "getlocation";}
+    @RequestMapping("/getlocation")
+    public String getlocation(HttpSession session) {
+
+//        List <POI> p = new ArrayList<>();
+//        POI p1 = new POI();
+//        POI p2 = new POI();
+//        POI p3 = new POI();
+//        p1.setPoipixx(39);
+//        p1.setPoipixy(116);
+////        p1.set
+////
+////        session.setAttribute();
+        return "getlocation";
+    }
 
 
 
